5b83957068cbc17c082159f340795418172f45ac
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2023-02-08 23:13:00 +0300

    ef is loaded and first migraion created

---
+++ DotNet7RPG/Data/DataContext.cs
@@ -0,0 +1,14 @@
+namespace DotNet7RPG.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    
+     
+
+    public DbSet<Character> Characters => Set<Character>();
+    
+
+}
--- DotNet7RPG/Program.cs
+++ DotNet7RPG/Program.cs
@@ -1,17 +1,22 @@
 global using DotNet7RPG.Models;
 global using DotNet7RPG.Services.CharacterService;
 global using DotNet7RPG.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+global using DotNet7RPG.Data;
 global using AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddScoped<ICharacterService, CharacterService>(); 
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
