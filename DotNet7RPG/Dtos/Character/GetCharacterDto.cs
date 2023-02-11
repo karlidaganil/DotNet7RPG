6ff6e553dd2a6882fb916110b02e20fef74fb343
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2023-02-12 00:05:22 +0300

    get set added fix

--- DotNet7RPG/Dtos/Character/GetCharacterDto.cs
+++ DotNet7RPG/Dtos/Character/GetCharacterDto.cs
@@ -9,5 +9,5 @@ public class GetCharacterDto
     public int Defense { get; set; } = 10;
     public int Intelligence { get; set; } = 10;
     public RpgClass Class { get; set; } = RpgClass.Knight;
-    public GetWeaponDto? Weapon;
+    public GetWeaponDto? Weapon { get; set; }
 }
